4f7f7633676ddd74491de5519c68db178da1b8a8
Diego Borrero <die.9511@gmail.com>
2018-10-09 15:45:58 -0500

    Part 1, toFix handleFileUpload

--- src/main/java/eci/cosw/AppConfiguration.java
+++ src/main/java/eci/cosw/AppConfiguration.java
@@ -4,21 +4,11 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.convert.CustomConversions;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 @Configuration
 public class AppConfiguration {
@@ -27,29 +17,19 @@ public class AppConfiguration {
     public MongoDbFactory mongoDbFactory() throws Exception {
 
         // Set credentials
-        MongoCredential credential = MongoCredential.createCredential("<username>", "<database-name>", "<password>".toCharArray());
-        ServerAddress serverAddress = new ServerAddress("ds149672.mlab.com", 49672);
+        MongoCredential credential = MongoCredential.createCredential("admin", "cosw-test", "admin0".toCharArray());
+        ServerAddress serverAddress = new ServerAddress("ds213338.mlab.com", 13338);
 
         // Mongo Client
         MongoClient mongoClient = new MongoClient(serverAddress, credential, new MongoClientOptions.Builder().build());
 
 
-        return new SimpleMongoDbFactory(mongoClient, "<database-name>");
+        return new SimpleMongoDbFactory(mongoClient, "cosw-test");
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-
         return new MongoTemplate(mongoDbFactory());
     }
 
-    @Bean
-    public GridFsTemplate gridFsTemplate() throws Exception {
-        MongoTemplate mongoTemplate = mongoTemplate();
-        return new GridFsTemplate(mongoDbFactory(), mongoTemplate.getConverter());
-    }
-
-
-
 }
-
--- src/main/java/eci/cosw/controller/RESTController.java
+++ src/main/java/eci/cosw/controller/RESTController.java
@@ -1,8 +1,16 @@
 package eci.cosw.controller;
 
 
+import com.mongodb.client.gridfs.model.GridFSFile;
 import eci.cosw.data.model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,20 +25,26 @@ public class RESTController {
 
 
    //TODO inject components (TodoRepository and GridFsTemplate)
+    @Autowired
+    GridFsTemplate gridFsTemplate;
 
     @RequestMapping("/files/{filename}")
     public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
 
-        //TODO implement method
-        return null;
-
+        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+        if (file == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(resource.getContentType()))
+                .body(new InputStreamResource(resource.getInputStream()));
     }
 
     @CrossOrigin("*")
     @PostMapping("/files")
     public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
-
-        //TODO implement method
+        gridFsTemplate.store(file.getInputStream(), file.getName(), file.getContentType());
         return null;
     }
 
